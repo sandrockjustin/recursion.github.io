f3115e5b8a3ffcf7e7cb87483c6619b6ddbf6181
sandrockjustin <sandrock.justin@gmail.com>
2024-06-10 15:58:17 -0500

    working on exponent

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -112,6 +112,23 @@ var range = function(x, y, output = []) {
 // Example:  exponent(4,3);  // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+
+  if (exp === 0){
+    return 1;
+  } else if (exp === 1){
+    return base;
+  }
+
+  if (exp > 1){
+
+    return base * exponent(base, exp - 1);
+
+  } else {
+
+    return base / exponent(base, exp + 1);
+
+  }
+
 };
 
 // 8. Determine if a number is a power of two.
