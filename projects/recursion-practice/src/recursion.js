ac3e2b3506737af1d70948bbd0ce8b9afa993304
sandrockjustin <sandrock.justin@gmail.com>
2024-06-10 14:51:42 -0500

    + commit factorial success

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -5,6 +5,15 @@
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
 var factorial = function(n) {
+
+  // base case
+  if (n === 0){
+    return 0;
+  }
+
+  // recursive statement
+  return n * factorial(n - 1);
+
 };
 
 // 2. Compute the sum of an array of integers.
